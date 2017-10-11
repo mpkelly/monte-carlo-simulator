1cdc06f8eb24866ed3e956b78d527b1260fd652d
Michael Kelly <michael@prologic.io>
2017-10-11 20:24:56 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Hello from "./Hello";
+
+ReactDOM.render(
+  <Hello name="Mike" />,
+  document.getElementById("app")
+);
---
+++ test/Hello.test.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {mount} from "enzyme";
+import { expect } from 'chai';
+import Hello from "../src/Hello";
+
+// describe("Hello", () => {
+//   it('renders a <Hello /> component', () => {
+//     const wrapper = mount(<Hello name="Mike" />);
+//     expect(wrapper.find("h1").text()).to.contain('Hello Mike');
+//   });
+// });
